9a60b1cd75e081b962950ba8c803fb2496fbfe14
George Vigelette <gvigelet@duvitech.com>
2015-06-14 16:26:56 -0400

    png

--- DemoProj/DisplayThread.c
+++ DemoProj/DisplayThread.c
@@ -25,11 +25,13 @@ PNG-Library   - ( )
 TrueTypeFonts - ( )
 ----------------------------------------------------------------------
 */
-
+#include <Windows.h>
 #include "GUI.h"
 #include "DisplayThread.h"
 #include "FirstpersonDataStructures.h"
 #include <stdbool.h>
+#include <math.h>
+#include <stdio.h>
 
 /*********************************************************************
 *
@@ -45,6 +47,8 @@ static bool bRunning = false;
 
 static bool Initialize(void);
 
+extern unsigned char nuke_png[8251];
+
 /*********************************************************************
 *
 *       Public code
@@ -58,12 +62,40 @@ static bool Initialize(void);
 void MainTask(void) {
 
 	bRunning = Initialize();
-	
-	GUI_DispString("Hello world!");
-	
+	int x0 = 160;
+	int y0 = 180;
+	int i;
+	char ac[4];
+	GUI_SetBkColor(GUI_WHITE);
+	GUI_Clear();
+	GUI_SetPenSize(5);
+	GUI_SetTextMode(GUI_TM_TRANS);
+	GUI_SetFont(&GUI_FontComic18B_ASCII);
+	GUI_SetColor(GUI_BLACK);
+	GUI_DrawArc(x0, y0, 150, 150, -30, 210);
+
+	for (i = 0; i <= 23; i++) {
+		float a = (-30 + i * 10)*3.1415926 / 180;
+		int x = -141 * cos(a) + x0;
+		int y = -141 * sin(a) + y0;
+		if (i % 2 == 0)
+			GUI_SetPenSize(5);
+		else
+			GUI_SetPenSize(4);
+		GUI_DrawPoint(x, y);
+		if (i % 2 == 0) {
+			x = -123 * cos(a) + x0;
+			y = -130 * sin(a) + y0;
+			sprintf(ac, "%d", 10 * i);
+			GUI_SetTextAlign(GUI_TA_VCENTER);
+			GUI_DispStringHCenterAt(ac, x, y);
+		}
+	}
+
+	GUI_PNG_Draw(nuke_png, 8251, 50, 50);
 	while (bRunning){
 
-		
+		Sleep(100);
 	}
 }
 
